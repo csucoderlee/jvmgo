fb1164ee317bec9f63239c9779e70e669e91406e
lixiang <csucoderlee@foxmail.com>
2018-07-09 23:30:18 +0800

    暂时提交

---
+++ ch02/classpath/classpath.go
@@ -0,0 +1,23 @@
+package classpath
+
+
+type Classpath struct {
+	bootClasspath Entry
+	extClasspath Entry
+	userClasspath Entry
+}
+
+func Parse(jreOption, cpOption string) *Classpath {
+	cp := &Classpath{}
+	cp.parseBootAndExtClasspath(jreOption)
+	cp.parseUserClasspath(cpOption)
+	return cp
+}
+
+func (self *Classpath) ReadClass(className string) ([]byte, Entry, error) {
+
+}
+
+func (self *Classpath) String() string {
+
+}
