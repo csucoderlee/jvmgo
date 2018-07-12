9238db234c7fe2770c2538f17ef6bbc43a216b27
lixiang <csucoderlee@foxmail.com>
2018-07-12 23:15:49 +0800

    startJVM启动入口

--- ch02/main.go
+++ ch02/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"jvmgo/ch02/classpath"
+	"strings"
 )
 
 func main() {
@@ -17,7 +19,16 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
 	fmt.Printf("classpath:%s class:%s args:%v\n",
 		cmd.cpOption, cmd.class, cmd.args)
+
+	className := strings.Replace(cmd.class, ".", "/",  -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("Could not find or load main class %s\n", cmd.class)
+		return
+	}
+	fmt.Printf("class data:%v\n", classData)
 }
 
